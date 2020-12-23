2a6004158624b0ce152ceb19a7283560e1d9f352
Vegard Bønes <vegard.bones@met.no>
2020-12-23 07:39:34 +0100

    fix image name generation

--- build_docker.sh
+++ build_docker.sh
@@ -2,11 +2,15 @@
 
 set -euo pipefail
 
+echo $@
+
 PREFIX="$1"
 NAME=$(dirname $2)
 VERSION=$(basename $2)
 
-IMAGE="${PREFIX}${NAME}:${VERSION}"
+IMAGE="${PREFIX}/${NAME}:${VERSION}"
+
+echo $IMAGE
 
 docker build -t "$IMAGE" -f "$NAME/build/package/Dockerfile" .
 
