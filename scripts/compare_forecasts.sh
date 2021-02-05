999c2c700d197138242ab0a1110d72825a9778ee
Vegard Bønes <vegard.bones@met.no>
2021-02-05 13:36:15 +0100

    add some extra possible locations

--- scripts/compare_forecasts.sh
+++ scripts/compare_forecasts.sh
@@ -5,8 +5,11 @@ set -euo pipefail
 SERVER_A=$1
 SERVER_B=$2
 
-URL_PATH="api/nowcast/v2/complete"
-POSITION="lat=60&lon=10"
+URL_PATH="/api/forecast/v2/complete"
+
+# POSITION="lat=60&lon=10"
+# POSITION="lat=0&lon=10"
+POSITION="lat=78.2167&lon=15.6333"
 
 FORMAT="jq ."
 
