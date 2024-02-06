03629fe4d2d7a994b329e17e4f236de96864392a
Vegard Bønes <vegardb@met.no>
2024-02-06 16:18:24 +0100

    add simple benchmarker tool

---
+++ benchmarker/internal/fortigrpc/fortigrpc.go
@@ -0,0 +1,67 @@
+package fortigrpc
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"gitlab.met.no/forti/f2/benchmarker/internal/location"
+	"gitlab.met.no/forti/f2/internalprotocol"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type FortiGRPC struct {
+	conn   *grpc.ClientConn
+	client internalprotocol.ForecasterClient
+}
+
+func NewClient(ctx context.Context, address string) (*FortiGRPC, error) {
+	conn, err := grpc.DialContext(
+		ctx, address,
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("did not connect: %w", err)
+	}
+	return &FortiGRPC{
+		conn:   conn,
+		client: internalprotocol.NewForecasterClient(conn),
+	}, nil
+}
+
+func (f *FortiGRPC) Close() error {
+	return f.conn.Close()
+}
+
+func (f *FortiGRPC) RandomRequest() (time.Duration, error) {
+	l := location.Pick()
+	request := internalprotocol.GetForecastRequest{
+		Latitude:  l.Latitude,
+		Longitude: l.Longitude,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	forecast, err := f.client.GetForecast(ctx, &request)
+	end := time.Now()
+
+	if err == nil {
+		interpreted := internalprotocol.InterpretValues(forecast)
+		ta, ok := interpreted["air_temperature_2m"]
+		if !ok {
+			log.Println("missing ta")
+		} else {
+			for i, v := range ta.Values {
+				if v > 60 {
+					log.Printf("%f/%f %v = %f", l.Latitude, l.Longitude, ta.Times[i], v)
+				}
+			}
+		}
+	}
+	return end.Sub(start), err
+}
---
+++ benchmarker/internal/fortihttp/fortihttp.go
@@ -0,0 +1,71 @@
+package fortihttp
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"gitlab.met.no/forti/f2/benchmarker/internal/location"
+)
+
+type FortiHTTP struct {
+	address   string
+	userAgent string
+	client    *http.Client
+	readDelay time.Duration
+}
+
+func NewClient(address string, readDelay time.Duration) (*FortiHTTP, error) {
+
+	if !strings.HasPrefix(address, "http") {
+		address = "http://" + address
+	}
+
+	return &FortiHTTP{
+		address:   address,
+		userAgent: "forti-benchamrker",
+		client: &http.Client{
+			Timeout:   5 * time.Second,
+			Transport: &http.Transport{},
+		},
+		readDelay: readDelay,
+	}, nil
+}
+
+func (f *FortiHTTP) Close() error {
+	return nil
+}
+
+func (f *FortiHTTP) RandomRequest() (time.Duration, error) {
+	l := location.Pick()
+
+	url := fmt.Sprintf("%s?lat=%.4f&lon=%.4f", f.address, l.Latitude, l.Longitude)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Add("User-Agent", f.userAgent)
+	req.Header.Add("Accept-Encoding", "gzip")
+
+	start := time.Now()
+	response, err := f.client.Do(req)
+	if err == nil {
+		if f.readDelay != 0 {
+			time.Sleep(f.readDelay)
+		}
+		_, err = io.ReadAll(response.Body)
+		response.Body.Close()
+
+		if response.StatusCode != 200 {
+			err = errors.New(response.Status)
+		}
+	}
+	end := time.Now()
+
+	return end.Sub(start), err
+}
